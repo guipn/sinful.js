79ce4100a4ed691f2bf0c06632d668f0a0ce7956
Danny Cook <mortchek@gmail.com>
2012-11-04 01:19:59 -0500

    Allow reducify to have an initial value.

--- test/Function.reducify.js
+++ test/Function.reducify.js
@@ -2,10 +2,21 @@ suite('Function.prototype.reducify');
 
 var a = require('assert');
 
+require('../sinful');
+
 test('Function.prototype.reducify on add', function(){
     
     var add = function(a, b){ return a + b },
         sum = add.reducify();
 
     a.equal(sum(1, 2, 3, 4), 10);
+});
+
+test('Function.prototype.reducify on add with initial value', function(){
+    
+    var add = function(a, b){ return a + b },
+        sum = add.reducify(0);
+
+    a.equal(sum(1, 2, 3, 4), 10);
+    a.equal(sum(), 0);
 });
